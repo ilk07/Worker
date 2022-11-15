a4807f8664ec988e8aa353d586336e7115a7c1bf
Ilia K <113442126+ilk07@users.noreply.github.com>
2022-11-15 22:01:21 +0300

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -7,16 +7,6 @@ public class Worker{
         this.errorCallback = errorCallback;
     }
 
-//    @FunctionalInterface
-//    public interface OnTaskDoneListener {
-//        void onDone(String result);
-//    }
-
-//    @FunctionalInterface
-//    public interface OnTaskErrorListener {
-//        void onError(String result);
-//    }
-
     public void start() {
         for (int i = 0; i < 100; i++) {
             if (i == 33) {
